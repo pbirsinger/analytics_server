bca6743cd31cbbd931f9d78b71c084a8a1d0871f
Peter Birsinger <pbirsinger@gmail.com>
2018-11-23 11:21:20 -0800

    cleanup

--- init_db.sh
+++ init_db.sh
@@ -5,3 +5,5 @@ createdb analytics_dev
 
 echo "CREATE USER postgres WITH SUPERUSER PASSWORD 'postgres'" | psql analytics_dev
 
+# CREATE INDEX event_ts ON "Events" USING BRIN (timestamp) WITH (pages_per_range = 128);
+# DROP INDEX event_ts;
--- src/server.ts
+++ src/server.ts
@@ -3,23 +3,6 @@ import { db, sequelize } from './models';
 
 import { getHourCounts, IEventInstance } from "./models/event";
 
-/**
-
-POST /analytics?timestamp={millis_since_epoch}&user={user_id}&event={click|impression}
-GET /analytics?timestamp={millis_since_epoch}
-
-When the POST request is made, a 204 is returned to the client with an empty response.
-We simply side-effect and track the event in our data store.
-
-When the GET request is made, we return information in the following format to the client,
-for the hour (assuming GMT time zone) of the requested timestamp:
-
-unique_users,{number_of_unique_usernames}
-clicks,{number_of_clicks}
-impressions,{number_of_impressions}
-
-**/
-
 const app = express();
 const port = 4040;
 
